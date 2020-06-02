421ed56ee09f32a5e0254a87160ae01846c660f7
airtonlopsan <lopsan@msn.com>
2020-06-02 18:24:12 -0300

    ajustes nos arquivos js

--- semana3/logica-booleana/Escrita1.js
+++ semana3/logica-booleana/Escrita1.js
@@ -1,6 +1,6 @@
 //1-
 //a-
-let fahKel = (77-32) * (5/9) + (273.15)
+/*let fahKel = (77-32) * (5/9) + (273.15)
 let respostaFahKel = ('77°F convertidos em Kelvin = ') + (fahKel) + (' K')
 console.log(respostaFahKel)
 
@@ -13,12 +13,12 @@ console.log(respostaCelFah)
 let celFah2 = (30) * (9/5) + (32)
 let respostaCelFah2 = ('30°C convertidos em Fahrenheit = ') + (celFah2 + ' C°')
 let respostaCelKel = (' e ') + ('convertidos em Kelvin são ') + (30 + 273.15) + (' K')
-console.log(respostaCelFah2+respostaCelKel)
+console.log(respostaCelFah2+respostaCelKel)*/
 
 //d-
-const valorUsuario = prompt ("Insira o valor em Celsius a ser convertido:")
-const celFah3 = (valorUsuario) * (9/5) + (32)
-const respostaCelFah3 = (valorUsuario) + (' convertidos em Fahrenheit = ') + (celFah3 + ' °F')
-const respostaCelKel2 = (' e ') + ('convertidos em Kelvin são ') + (valorUsuario) + (273.15) + (' K')
-let respostaFinal = respostaCelFah3+ respostaCelKel2
-console.log(respostaFinal)
+let valorUsuario = Number.parseInt(prompt ('Insira o valor em Celsius a ser convertido:'))
+let celFah2 = valorUsuario * (9/5) + (32)
+let respostaCelFah2 = (valorUsuario) + (' convertidos em Fahrenheit são ') + (celFah2 + ' C°!')
+let respostaCelKel3 = ('\nE ') + (valorUsuario) + (' convertidos em Kelvin são ')
+let respostaCelKel1 = (273.15) + (valorUsuario)
+console.log(respostaCelFah2 + respostaCelKel3 + respostaCelKel1)
