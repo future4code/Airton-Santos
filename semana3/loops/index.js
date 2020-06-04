b9c76a7bf474ce509187459842df6c962d8963de
airtonlopsan <lopsan@msn.com>
2020-06-04 16:54:45 -0300

    exercicios do dia nao realizados por completo, faltando 3b e 3c e 3d

---
+++ semana3/loops/index.js
@@ -0,0 +1,53 @@
+/*EXERCICIO 1
+
+O que o código abaixo está fazendo?
+
+O codigo esta criando um loop para que a variavel sum, some ele mesmo,
+enquanto o resultado for menor que 15
+
+Qual o resultado impresso no console?
+
+105*/
+
+/*EXERCICIO 2
+
+a. O que o comando .push faz?
+
+Cria o array novaLista, apenas com os indices do array lista que for divisivel por 5
+
+b. Qual o valor impresso no console?
+
+[10, 15, 25, 30]
+
+c. Qual seria imprimido no console se a variável numero tivesse o valor de 3? E se tivesse o valor de 4?
+
+numero = 3 :[12, 15, 18, 21, 27, 30]
+numero = 4 :[12]                      */
+
+//*EXERCICIO 3*//
+//a
+/*let arrayOriginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let maiorNumero = 0
+let menorNumero = 999
+
+for(numeroDoArray of arrayOriginal) {
+    if (numeroDoArray < menorNumero) {
+        menorNumero=numeroDoArray
+        }
+
+        if (numeroDoArray > maiorNumero) {
+            maiorNumero=numeroDoArray
+        }
+
+}
+
+console.log("O menor numero e ", menorNumero, "e o maior numero e ", maiorNumero)*/
+
+//b
+const lista = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+const novaLista = []
+for(const item of lista) {
+    (item/10)
+    novaLista.push(Number(item))
+    console.log(novaLista)
+  }
