c02a48a7f19ed8f6404f35c1f7290f97d0662c53
airtonlopsan <lopsan@msn.com>
2020-06-09 14:46:21 -0300

    Planner feito, falta estilizar

---
+++ semana4/planner/index.js
@@ -0,0 +1,58 @@
+function adicionaItem() {
+    let meuInput = document.getElementById("meu-input")
+    let item = meuInput.value
+
+    let meuDia = document.getElementById("selecao-dias")
+    let selecaoDias = meuDia.value
+
+    let segunda = document.getElementById("option-segunda")
+    let segundaFeira = segunda.value
+
+    let terca = document.getElementById("option-terca")
+    let tercaFeira = terca.value
+
+    let quarta = document.getElementById("option-quarta")
+    let quartaFeira = quarta.value
+
+    let quinta = document.getElementById("option-quinta")
+    let quintaFeira = quinta.value
+
+    let sexta = document.getElementById("option-sexta")
+    let sextaFeira = sexta.value
+
+    let sabado = document.getElementById("option-sabado")
+    let diaSabado = sabado.value
+
+    let domingo = document.getElementById("option-domingo")
+    let diaDomingo = domingo.value
+
+    if (selecaoDias === segundaFeira) {
+    let tarefaDia = document.getElementById("dia-segunda")
+    tarefaDia.innerHTML += `<li> ${item} </li>`
+    meuInput.value = ""
+    } if (selecaoDias === tercaFeira) {
+        let tarefaDia = document.getElementById("dia-terca")
+        tarefaDia.innerHTML += `<li> ${item} </li>`
+        meuInput.value = ""
+        } if (selecaoDias === quartaFeira) {
+            let tarefaDia = document.getElementById("dia-quarta")
+            tarefaDia.innerHTML += `<li> ${item} </li>`
+            meuInput.value = ""
+        } if (selecaoDias === quintaFeira) {
+            let tarefaDia = document.getElementById("dia-quinta")
+            tarefaDia.innerHTML += `<li> ${item} </li>`
+            meuInput.value = ""
+        } if (selecaoDias === sextaFeira) {
+            let tarefaDia = document.getElementById("dia-sexta")
+            tarefaDia.innerHTML += `<li> ${item} </li>`
+            meuInput.value = ""
+        } if (selecaoDias === diaSabado) {
+            let tarefaDia = document.getElementById("dia-sabado")
+            tarefaDia.innerHTML += `<li> ${item} </li>`
+            meuInput.value = ""
+        } if (selecaoDias === diaDomingo) {
+            let tarefaDia = document.getElementById("dia-domingo")
+            tarefaDia.innerHTML += `<li> ${item} </li>`
+            meuInput.value = ""
+        }
+}
