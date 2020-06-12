54e272bde995fca658e9cfff4d6c6a083b893249
airtonlopsan <lopsan@msn.com>
2020-06-12 16:56:50 -0300

    Exercicios feitos de forma parcial

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -8,6 +8,30 @@
 //escreverei 5 no prompt, e o console imprimira R$500
 
 //2-
-//O que ele faz?
-//Como faz?
+//O que ele faz? Esse codigo compara dois investimentos distintos
+//Como faz? E uma funcao que solicita quatro valores: Uma opcao de investimento,
+//e o valor que se pretende investir, e outra opcao de investimento, e valor
+//e devolve ao usuario seus respectivos ganhos para que veja qual rende mais.
+//Alem disso, se o usuario nao escolher nenhuma das opcoes do switch/case,
+//ira retornar um alert, que o tipo de investimento esta incorreto.
+//Qual sera o valor impresso no console:
+//165
+//Alerta: TIPO DE INVESTIMENTO INCORRETO!
+
+//3-
+//O que ele faz? Retorna quantos numeros pares e quantos numeros impares tem
+//no array numeros.
+//Como faz? Primeiro, declarando a variavel numeros contendo o array de numeros,
+//e mais duas variaveis, array1 e array2, que receberao valores e formarao um
+//novo array. Segundo, temos um laco do tipo "for of" que verifica SE o resto
+//da divisao por 2 e igual a 0 (par) fara um push para o array1, SE NAO, fara
+//um push para o array2.
+//Qual sera o valor impresso no console:
+//Quantidade total de numeros[14]
+//[6]
+//[8]
+
+//4-
+//O que ele faz? 
+//Como faz? 
 //Qual sera o valor impresso no console:
