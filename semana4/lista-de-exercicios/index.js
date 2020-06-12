d3ba777e8e639f24190dd7ae4b865a5fab516f69
airtonlopsan <lopsan@msn.com>
2020-06-12 13:43:27 -0300

    lista-de-exercicios

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,13 @@
+//Exercícios de interpretação de código
+//1-
+//O que ele faz? Esse codigo converte 100 reais para Dolar informormando
+//a cotacao atual do Dolar em Reais.
+//Como faz? E uma funcao que solicita um input do usuario, da cotacao do dolar,
+//ja converte em numero com o Number, e multiplica pela variavel valorEmDolar.
+//Qual sera o valor impresso no console: Se a cotacao for por exemplo, 5 reais,
+//escreverei 5 no prompt, e o console imprimira R$500
+
+//2-
+//O que ele faz?
+//Como faz?
+//Qual sera o valor impresso no console:
