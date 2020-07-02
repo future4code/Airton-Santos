97c85d511a4517855dd426b783dd36bd75fec102
airtonlopsan <lopsan@msn.com>
2020-07-02 19:07:59 -0300

    Tarefa realizada

--- semana7/geramemes-api/src/App.js
+++ semana7/geramemes-api/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import axios from 'axios'
 
 const DivPrincipal = styled.div `
 background-color: #282c34;
@@ -10,12 +11,25 @@ align-items: center;
 font-size: calc(10px + 2vmin);
 color: white;
 `
-const ImgMeme = styled.img `
+const DivSecundaria = styled.div `
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+const ImgPokemon = styled.img `
 max-width: 80vw;
 max-height: 70vh;
 `
-const ButtonGeraMeme = styled.button `
+const ButtonGeraPokemon = styled.button `
 margin-bottom: 10vh;
+font-weight: bold;
+color:#fff;
+font-size: 0.8em;
+background-color: green;
+outline: none;
+cursor: pointer;
+border: none;
+border-radius: 8px;
 `
 const H5 = styled.h5 `
 text-align: center;
@@ -23,34 +37,57 @@ text-align: center;
 
 class App extends React.Component {
   state = {
-    meme: "",
-    "memes": [
-              {
-                "id": "131087935",
-                "name": "Running Away Balloon",
-                "url": "https://i.imgflip.com/261o3j.jpg",
-                "width": 761,
-                "height": 1024,
-                "box_count": 5
-              }
-            ]
+    imgPokemon: "",
+    pokemonList: [],
+    numeroAleatorio: 5,
+    todosPokemons: [],
+    namePokemon: ""
   }
 
-  
-  renderizaMeme = () => {
-  const meme = this.state.memes.map(imagem => {
-    return <ImgMeme src={imagem.url} />
-  })
-  this.setState({meme: meme})
-}
 
+  componentDidMount = () => {
+    this.guardaPokemons()
+    this.exibePokemon()
+  };  
+  
+  guardaPokemons = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/?limit=500`)
+      .then(response => {
+        this.setState({ todosPokemons: response.data.results});
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
+  exibePokemon = () => {
+    let min = 1
+    let max = 500
+    let rand = min + Math.floor(Math.random() * (max-min))
+
+    console.log(rand)
+
+   axios
+  .get(`https://pokeapi.co/api/v2/pokemon/${rand}`)
+  .then(response => {
+    this.setState({ imgPokemon: response.data.sprites.front_default});
+    this.setState({ namePokemon: response.data.name});
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
+}
   render() {
 
   return (
     <DivPrincipal>
-      <H5>Clique no botao abaixo para gerar um template aleatorio para seus memes!</H5>
-      <ButtonGeraMeme onClick={this.renderizaMeme}>GERAR</ButtonGeraMeme>
-      <div>{this.state.meme}</div>
+      <H5>Clique no botao abaixo para gerar uma foto de pokemon aletorio!</H5>
+      <ButtonGeraPokemon onClick={this.exibePokemon}>GERAR</ButtonGeraPokemon>
+      <DivSecundaria>
+        <img src={this.state.imgPokemon} alt="pokemon" />
+        <h4>{this.state.namePokemon}</h4>
+      </DivSecundaria>
     </DivPrincipal>
   );
   }
