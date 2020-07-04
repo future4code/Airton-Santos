eebaa1fb63cb894036d076433edbfb5d2e83aa36
airtonlopsan <lopsan@msn.com>
2020-07-04 10:38:44 -0300

    projeto Labefy em desencolvimento

--- semana7/labefy-projeto/src/components/MusicsLists/style.js
+++ semana7/labefy-projeto/src/components/MusicsLists/style.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
 export const MusicsListContainer = styled.div`
-background-color: #503030;
+    background-color: #503030;
+    > h1
+        {
+        color: #fff;
+        background-color: black;
+        }
 `
