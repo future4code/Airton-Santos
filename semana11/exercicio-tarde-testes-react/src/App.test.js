a4118ca303e900bb37f18dc555a6b052c63c7786
airtonlopsan <lopsan@msn.com>
2020-07-28 17:38:36 -0300

    feito ate o exercicio 2

---
+++ semana11/exercicio-tarde-testes-react/src/App.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { render, fireEvent, getByDisplayValue, getByText } from "@testing-library/react";
+import App from "./App";
+
+describe("Ao digitar no campo novo post e clicar em adicionar, e esperado que apareca um post com o que o usuario digitou", () => {
+    test("Input existe na tela", () => {
+        
+        const {getByPlaceholderText} = render(<App />)
+
+        const input = getByPlaceholderText("Novo post")
+
+        expect(input).toBeInTheDocument()
+    })
+
+    test("Botao existe na tela", () => {
+        const {getByText} = render(<App />)
+
+        expect(getByText(/Adicionar/)).toBeInTheDocument()
+    })
+
+    test("Post aparece na tela", () => {
+        const {getByPlaceholderText, getByText} = render(<App />)
+
+        const input = getByPlaceholderText("Novo post")
+
+        fireEvent.change(input, { target: { value: "post test" }})
+
+        const button = getByText(/Adicionar/)
+
+        fireEvent.click(button)
+
+        expect(getByText(/post test/)).toBeInTheDocument()
+    })
+})
+
+describe("Ao clicar no botao curtir, o botao deve mudar para descurtir", () => {
+    test("Botao curtir e clicado", () => {
+        const {getByPlaceholderText, getByText} = render(<App />)
+
+        const input = getByPlaceholderText("Novo post")
+        const Addbutton = getByText(/Adicionar/)
+
+        fireEvent.change(input, { target: { value: "post test" }})
+        fireEvent.click(Addbutton)
+                
+        const Likebutton = getByText(/Curtir/i)
+
+        fireEvent.click(Likebutton)
+
+        expect(getByText(/post test/)).toBeInTheDocument()
+        expect(getByText(/Descurtir/i)).toBeInTheDocument()
+    })
+})
