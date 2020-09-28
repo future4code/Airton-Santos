5c8d4a67232c0e20098f7f4c2a471c112148a2bb
airtonlopsan <lopsan@msn.com>
2020-09-28 16:45:28 -0300

    exercicios de segunda - testes

---
+++ semana18/exercicios-segunda-testes/tests/exercicios-segunda.test.js
@@ -0,0 +1,13 @@
+test("Testing balance greater than value", () => {
+	const user: User = {
+		name: "Astrodev",
+		balance: 100
+	}
+
+	const result = performPurchase(user, 50)
+	
+	expect(result).toEqual({
+		...user,
+		balance: 50
+	})
+})
