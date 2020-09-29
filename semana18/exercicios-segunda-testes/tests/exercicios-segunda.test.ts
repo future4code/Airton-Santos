e839a56127981e679dfbc882cb6cf149c58f5ca7
airtonlopsan <lopsan@msn.com>
2020-09-29 02:06:12 -0300

    não consegui fazer exercícios 6 e 7

--- semana18/exercicios-segunda-testes/tests/exercicios-segunda.test.ts
+++ semana18/exercicios-segunda-testes/tests/exercicios-segunda.test.ts
@@ -1,4 +1,3 @@
-import { createPost } from '../src/controller/createPost';
 import {Casino, LOCATION, NACIONALITY, performPurchase, User, UserClient, verifyAge} from '../src/index'
 
 describe('testes para a função de conta do usuário', () => {
@@ -225,29 +224,4 @@ describe('testes da função do exercício 3', () => {
         expect(result.americans.unallowed.length).toBeLessThan(1);
         expect(result.americans.allowed.length).toBe(2);
     });
-});
-
-describe('testes das funções assíncronas', () => {
-    test("Create Post", async () => {
-        const post = {
-        id: "id do post",
-        title: "Título",
-        content: "Conteúdo",
-        };
-    
-        await createPost(post);
-        const postFromDb = await getPostById(post.id);
-    
-        expect(postFromDb).not.toBe(undefined);
-        expect(postFromDb).toEqual({
-        id: "id do post",
-        title: "Título",
-        content: "Conteúdo",
-        });
-    });
-
-    // afterAll(async () => {
-    //     await deletePostById("id do post");
-    //     await destroyConnection();
-    // });
 });
