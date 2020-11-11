5ea3ad76bf86d4184d18abbe3d5e3ea911d1564f
airtonlopsan <lopsan@msn.com>
2020-11-11 17:51:26 -0300

    tarefa não concluída

---
+++ logic-exercises/sumTwoNumbers/src/index.ts
@@ -0,0 +1,14 @@
+export function sumTwoNumbers(nums: number[], target: number) {
+    let indexOfNums: number[] = [];
+
+    for (let i = 0,  !== 0, ) {
+        indexOfNums.push(i)
+    }
+    return 
+}
+
+let numbers: number[] = [44, 55, 66, 77]
+
+console.log(sumTwoNumbers(numbers, 100))
+
+console.log(numbers.indexOf(66))
